71d612fdf0ff181e3503015eb03537d2386a2529
Isabela <isabelaolsemann@gmail.com>
2025-11-22 23:52:57 -0300

    project was created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import GlobalStyle from './global'
+import { Botao } from './global'
+
+import temaLight from './themes/light'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaLight}>
+      <GlobalStyle />
       <Header />
       <Hero />
       <div className="container">
         <ListaVagas />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/themes/dark.ts
@@ -0,0 +1,7 @@
+const temaDark = {
+  corPrincipal: '#c795a1',
+  corSecundaria: '#2c2c2c',
+  corDeFundo: '#121212'
+}
+
+export default temaDark
